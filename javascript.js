3cf2a1a1a23140daff667573d36019d9fe2a5ede
TravisIPeterson <travisianpeterson@gmail.com>
2023-03-13 15:32:47 -0700

    Style with CSS and insert DOM manipulation for classes

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
 let size;
 
 const container = document.querySelector('.container');
+const controlPanel = document.createElement('div');
+const sizeButton = document.createElement('button');
+const body = document.querySelector('body');
 
-size = parseInt(prompt("How big do you want this time to be? (Number input will represent each side of the perimeter, i.e. 10 would make 100 squares)"));
+controlPanel.className = "panel";
+body.appendChild(controlPanel);
+sizeButton.textContent = "How big do you want this time to be? (Number input will represent each side of the perimeter, i.e. 10 would make 100 squares)"
+controlPanel.appendChild(sizeButton);
+
+size = parseInt(prompt("How many squares?", "16"));
     if (size > 100) {
-        window.alert("Whoa there, friend! You might want to slow down. Doing anything bigger than a 100 x 100 square would really take a toll on your computer.")
+        window.alert("Whoa there, friend! You might need to slow down. Doing anything bigger than a 100 x 100 square would really take a toll on your computer.");
     }
 
     else {
         for (i = 0; i < size * size; i++) {
             const box = document.createElement('div');
             box.className = "box";
+            container.appendChild(box);
             box.addEventListener('mouseover', function(e) {
                 box.classList.add('hover');
                 box.classList.remove('leave');
